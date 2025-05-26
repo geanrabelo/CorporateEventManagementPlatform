68d03c6f2deae0be54463b4b791c8245f63bfdbd
geanrabelo <geancardosorabelomota@gmail.com>
2025-05-26 12:18:07 -0300

    updated auth

--- src/main/java/com/br/CEMP/service/impl/UserServiceImpl.java
+++ src/main/java/com/br/CEMP/service/impl/UserServiceImpl.java
@@ -1,4 +1,29 @@
 package com.br.CEMP.service.impl;
 
-public class UserServiceImpl {
+import com.br.CEMP.dto.AuthenticationDTO;
+import com.br.CEMP.dto.RegisterDTO;
+import com.br.CEMP.repository.UserRepository;
+import com.br.CEMP.service.interfaces.UserService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public String login(AuthenticationDTO authenticationDTO) {
+
+        return "";
+    }
+
+    @Override
+    public String register(RegisterDTO registerDTOe) {
+
+        return "";
+    }
 }
--- src/main/java/com/br/CEMP/util/filter/SecurityFilter.java
+++ src/main/java/com/br/CEMP/util/filter/SecurityFilter.java
@@ -1,4 +1,48 @@
 package com.br.CEMP.util.filter;
 
-public class SecurityFilter {
+import com.br.CEMP.service.AuthenticationService;
+import com.br.CEMP.service.TokenService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    private final TokenService tokenService;
+
+    private final AuthenticationService authenticationService;
+
+    public SecurityFilter(TokenService tokenService, AuthenticationService authenticationService){
+        this.tokenService = tokenService;
+        this.authenticationService = authenticationService;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = recoverToken(request);
+        if(token != null){
+            String email = tokenService.validateToken(token);
+            UserDetails user = authenticationService.loadUserByUsername(email);
+
+            var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String recoverToken(HttpServletRequest httpServletRequest){
+        String token = httpServletRequest.getHeader("Authorization ");
+        if(token == null) return null;
+        return token.replace("Bearer ", "");
+    }
 }
