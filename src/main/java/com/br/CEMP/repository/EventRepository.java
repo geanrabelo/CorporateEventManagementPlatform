9d82b0dcc5128ce0960e759c28417cf88597fd1c
geanrabelo <geancardosorabelomota@gmail.com>
2025-05-26 11:32:07 -0300

    create EventRepository

---
+++ src/main/java/com/br/CEMP/repository/EventRepository.java
@@ -0,0 +1,9 @@
+package com.br.CEMP.repository;
+
+import com.br.CEMP.model.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface EventRepository extends JpaRepository<Event, UUID> {
+}
