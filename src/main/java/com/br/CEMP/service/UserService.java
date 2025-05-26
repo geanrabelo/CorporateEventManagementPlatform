c85fd2a2c37ec6f5d0b124e6b271eab3fe71234c
geanrabelo <geancardosorabelomota@gmail.com>
2025-05-26 11:39:39 -0300

    create UserService implements UserDetailsService

--- src/main/java/com/br/CEMP/service/UserService.java
+++ src/main/java/com/br/CEMP/service/UserService.java
@@ -1,4 +1,22 @@
 package com.br.CEMP.service;
 
-public class UserService {
+import com.br.CEMP.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email);
+    }
 }
