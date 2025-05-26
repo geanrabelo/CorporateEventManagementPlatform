5100d2e3148518c2e35e35fadac26b54e214954e
geanrabelo <geancardosorabelomota@gmail.com>
2025-05-26 15:38:37 -0300

    update EnumCode - EVT000, EVT001

--- src/main/java/com/br/CEMP/model/enums/EnumCode.java
+++ src/main/java/com/br/CEMP/model/enums/EnumCode.java
@@ -3,7 +3,9 @@ package com.br.CEMP.model.enums;
 public enum EnumCode {
     USR000("Already registered this username"),
     USR001("Already registered this email"),
-    USR003("User find by id not found");
+    USR003("User find by id not found"),
+    EVT000("Already exists event with tittle"),
+    EVT001("Event find by id not found");
 
     private String message;
 
