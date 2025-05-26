9efe36ace6a24e7f164c1b3a7d756f8b7ff08365
geanrabelo <geancardosorabelomota@gmail.com>
2025-05-26 15:36:59 -0300

    create exception - EventAlreadyExistsByTittle

---
+++ src/main/java/com/br/CEMP/exceptions/ex/EventAlreadyExistsByTittle.java
@@ -0,0 +1,7 @@
+package com.br.CEMP.exceptions.ex;
+
+public class EventAlreadyExistsByTittle extends RuntimeException {
+  public EventAlreadyExistsByTittle(String message) {
+    super(message);
+  }
+}
