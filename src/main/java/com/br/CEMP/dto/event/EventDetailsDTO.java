c41bad68cd9187e1d98d004bdc3ee46fc11be9a8
geanrabelo <geancardosorabelomota@gmail.com>
2025-05-26 15:28:38 -0300

    create EventDetailsDTO

---
+++ src/main/java/com/br/CEMP/dto/event/EventDetailsDTO.java
@@ -0,0 +1,24 @@
+package com.br.CEMP.dto.event;
+
+import com.br.CEMP.model.Event;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+public record EventDetailsDTO(UUID id,
+                              String tittle,
+                              String description,
+                              LocalDate date,
+                              String objective,
+                              Long responsible_id) {
+    public EventDetailsDTO(Event event){
+        this(
+                event.getId(),
+                event.getTittle(),
+                event.getDescription(),
+                event.getDate(),
+                event.getObjective(),
+                event.getResponsible().getId()
+        );
+    }
+}
