7d1899b3ea165edd5008a6ff7bea1931153cd18f
geanrabelo <geancardosorabelomota@gmail.com>
2025-05-26 12:23:56 -0300

    updated UserRepository - existsByUsername and existsByEmail

--- src/main/java/com/br/CEMP/repository/UserRepository.java
+++ src/main/java/com/br/CEMP/repository/UserRepository.java
@@ -7,4 +7,8 @@ import org.springframework.security.core.userdetails.UserDetails;
 public interface UserRepository extends JpaRepository<User, Long> {
 
     UserDetails findByEmail(String email);
+
+    boolean existsByUsername(String username);
+
+    boolean existsByEmail(String email);
 }
