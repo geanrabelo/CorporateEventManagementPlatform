b1cb7d38b969ad6af30cd9d6c07a0db0a3a1eaf9
geanrabelo <geancardosorabelomota@gmail.com>
2025-05-26 14:35:10 -0300

    updated ErrorResponse - conflict

--- src/main/java/com/br/CEMP/exceptions/dto/ErrorResponse.java
+++ src/main/java/com/br/CEMP/exceptions/dto/ErrorResponse.java
@@ -1,4 +1,18 @@
 package com.br.CEMP.exceptions.dto;
 
-public record ErrorResponse() {
+import org.springframework.http.HttpStatus;
+
+import java.util.List;
+
+public record ErrorResponse(int status,
+                            String message,
+                            List<FieldError> fieldErrorList) {
+
+    public static ErrorResponse notFound(String message){
+        return new ErrorResponse(HttpStatus.NOT_FOUND.value(), message, List.of());
+    }
+
+    public static ErrorResponse conflict(String message){
+        return new ErrorResponse(HttpStatus.CONFLICT.value(), message, List.of());
+    }
 }
