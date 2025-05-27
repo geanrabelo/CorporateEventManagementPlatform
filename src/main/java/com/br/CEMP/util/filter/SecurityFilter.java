75a45578ae3fd1e8b443515de2d1f29dad71e556
geanrabelo <geancardosorabelomota@gmail.com>
2025-05-27 18:23:36 -0300

    update SecurityFilter - extract roles of DecodeJwt provided of validateToken

--- src/main/java/com/br/CEMP/util/filter/SecurityFilter.java
+++ src/main/java/com/br/CEMP/util/filter/SecurityFilter.java
@@ -1,5 +1,7 @@
 package com.br.CEMP.util.filter;
 
+import com.auth0.jwt.interfaces.Claim;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import com.br.CEMP.service.AuthenticationService;
 import com.br.CEMP.service.TokenService;
 import jakarta.servlet.FilterChain;
@@ -7,12 +9,15 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.List;
 
 @Component
 public class SecurityFilter extends OncePerRequestFilter {
@@ -30,18 +35,24 @@ public class SecurityFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String token = recoverToken(request);
         if(token != null){
-            String username = tokenService.validateToken(token);
-            UserDetails user = authenticationService.loadUserByUsername(username);
+            DecodedJWT decodedJWT = tokenService.validateToken(token);
 
-            var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+            if(decodedJWT != null){
+                String username = decodedJWT.getSubject();
+                String roles = decodedJWT.getClaim("roles").asString();
 
-            SecurityContextHolder.getContext().setAuthentication(authentication);
+                List<SimpleGrantedAuthority> authorities = List.of(new SimpleGrantedAuthority("ROLE_" + roles));
+
+                var authentication = new UsernamePasswordAuthenticationToken(username, null, authorities);
+
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
         }
         filterChain.doFilter(request, response);
     }
 
     private String recoverToken(HttpServletRequest httpServletRequest){
-        String token = httpServletRequest.getHeader("Authorization ");
+        String token = httpServletRequest.getHeader("Authorization");
         if(token == null) return null;
         return token.replace("Bearer ", "");
     }
