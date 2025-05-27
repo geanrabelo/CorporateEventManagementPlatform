367aa93e7ffe70e0a6f62a4bd37078ae2d973f63
geanrabelo <geancardosorabelomota@gmail.com>
2025-05-27 11:39:08 -0300

    update EventUpdateDTO - id was UUID, now is String

--- src/main/java/com/br/CEMP/dto/event/EventUpdateDTO.java
+++ src/main/java/com/br/CEMP/dto/event/EventUpdateDTO.java
@@ -1,9 +1,8 @@
 package com.br.CEMP.dto.event;
 
 import java.time.LocalDate;
-import java.util.UUID;
 
-public record EventUpdateDTO(UUID id,
+public record EventUpdateDTO(String id,
                              String tittle,
                              String description,
                              LocalDate date,
