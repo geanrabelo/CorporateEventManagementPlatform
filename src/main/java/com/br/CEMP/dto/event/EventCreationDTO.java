227711ebbf4be3ce0a57d2577c1ef9d59ceb9f65
geanrabelo <geancardosorabelomota@gmail.com>
2025-05-27 11:14:29 -0300

    update EventCreationDTO - added @NotBlank in attributes

--- src/main/java/com/br/CEMP/dto/event/EventCreationDTO.java
+++ src/main/java/com/br/CEMP/dto/event/EventCreationDTO.java
@@ -2,13 +2,18 @@ package com.br.CEMP.dto.event;
 
 import com.br.CEMP.model.User;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotBlank;
 
 import java.time.LocalDate;
 
-public record EventCreationDTO(String tittle,
+public record EventCreationDTO(@NotBlank(message = "Title cannot be blank.")
+                               String tittle,
+                               @NotBlank(message = "Description cannot be blank.")
                                String description,
-                               @JsonFormat(pattern = "yyyy-MM-dd")
+                               @JsonFormat(pattern = "yyyy-MM-dd") @NotBlank(message = "Date cannot be blank.")
                                LocalDate date,
+                               @NotBlank(message = "Objective cannot be blank.")
                                String objective,
+                               @NotBlank(message = "Responsible cannot be blank.")
                                Long responsible_id) {
 }
