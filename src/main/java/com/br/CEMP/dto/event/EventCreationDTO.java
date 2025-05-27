fe6e7ab51752be65093d8e176dce8b471f4b794e
geanrabelo <geancardosorabelomota@gmail.com>
2025-05-27 12:24:36 -0300

    update EventCreationDTO - trade LocalDate to LocalDateTime

--- src/main/java/com/br/CEMP/dto/event/EventCreationDTO.java
+++ src/main/java/com/br/CEMP/dto/event/EventCreationDTO.java
@@ -3,14 +3,15 @@ package com.br.CEMP.dto.event;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotBlank;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public record EventCreationDTO(@NotBlank(message = "Title cannot be blank.")
                                String tittle,
                                @NotBlank(message = "Description cannot be blank.")
                                String description,
-                               @JsonFormat(pattern = "yyyy-MM-dd") @NotBlank(message = "Date cannot be blank.")
-                               LocalDate date,
+                               @JsonFormat(pattern = "yyyy-MM-dd")
+                               @NotBlank(message = "Date cannot be blank.")
+                               LocalDateTime date,
                                @NotBlank(message = "Objective cannot be blank.")
                                String objective,
                                @NotBlank(message = "Responsible cannot be blank.")
